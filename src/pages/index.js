5484d047f86fbc3e40fc5fa065ec3a9d64fdaccf
Keith Ssewanonda <keith@laboremus.no>
2020-06-04 12:06:53 +0300

    feat: add data markers

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import L from 'leaflet';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
@@ -8,8 +9,8 @@ import Map from 'components/Map';
 import axios from 'axios';
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369,
+  lat: 0,
+  lng: 0,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -55,6 +56,58 @@ const IndexPage = () => {
       })
     }
     console.log(geoJson)
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+    
+        const {
+          country,
+          updated,
+          cases,
+          deaths,
+          recovered
+        } = properties
+    
+        casesString = `${cases}`;
+    
+        if ( cases > 1000 ) {
+          casesString = `${casesString.slice(0, -3)}k+`
+        }
+    
+        if ( updated ) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+    
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${ casesString }
+          </span>
+        `;
+    
+        return L.marker( latlng, {
+          icon: L.divIcon({
+            className: 'icon',
+            html
+          }),
+          riseOnHover: true
+        });
+      }
+    });
+
+  geoJsonLayers.addTo(map)
+
   }
 
   const mapSettings = {
